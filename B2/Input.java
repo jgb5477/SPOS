46027b34ed05d9fc8f91d29c9bb1509f25ebefc0
Jayesh103 <jgb5477@gmail.com>
2021-06-06 19:53:06 +0530

    master

---
+++ B2/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input{
+    public static void main(String args[]){
+        int a5 = 10;
+        int temp = a5+5;
+        /*Multi Line
+        Comment*/
+        System.out.println("Hello world");
+        //Single Line Comment
+    }
+}
